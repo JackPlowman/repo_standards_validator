b2b287c141301ec0a0e9fdbbdffda1ad489a2fe2
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-31 21:18:31 +0000

    update

--- validator/__main__.py
+++ validator/__main__.py
@@ -6,7 +6,6 @@ from structlog import get_logger, stdlib
 from .configuration import Configuration
 from .custom_logging import set_up_custom_logging
 from .repositories import retrieve_repositories
-logger: stdlib.BoundLogger = get_logger()
 
 logger: stdlib.BoundLogger = get_logger()
 
@@ -15,6 +14,6 @@ def main() -> None:
     set_up_custom_logging()
     configuration = Configuration()
     _repositories = retrieve_repositories(configuration)
-    
+
 if __name__ == "__main__":
     main()
