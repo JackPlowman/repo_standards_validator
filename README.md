764b14a3ee1b411227654105ac303b05cd63637e
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-31 21:07:36 +0000

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+## Validator
+
+### How to run validator
+
+`poetry run python3 -m validator`
--- validator/__main__.py
+++ validator/__main__.py
@@ -7,3 +7,11 @@ from structlog import get_logger, stdlib
 from .custom_logging import set_up_custom_logging
 
 logger: stdlib.BoundLogger = get_logger()
+
+def main() -> None:
+    """Application entry point."""
+    set_up_custom_logging()
+    logger.info("Hello, World!")
+
+if __name__ == "__main__":
+    main()
