d576995efd5da6705aeedcf8f791b073d98ad499
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-31 20:27:46 +0000

    update

--- validator/__main__.py
+++ validator/__main__.py
@@ -3,15 +3,18 @@
 
 from structlog import get_logger, stdlib
 
-
+from .configuration import Configuration
 from .custom_logging import set_up_custom_logging
+from .repositories import retrieve_repositories
+logger: stdlib.BoundLogger = get_logger()
 
 logger: stdlib.BoundLogger = get_logger()
 
 def main() -> None:
     """Application entry point."""
     set_up_custom_logging()
-    logger.info("Hello, World!")
-
+    configuration = Configuration()
+    _repositories = retrieve_repositories(configuration)
+    
 if __name__ == "__main__":
     main()
---
+++ validator/repositories.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+
+from typing import TYPE_CHECKING
+
+
+from github import Github, PaginatedList, Repository
+from structlog import get_logger, stdlib
+
+if TYPE_CHECKING:
+    from .configuration import Configuration
+
+logger: stdlib.BoundLogger = get_logger()
+
+
+def retrieve_repositories(configuration: Configuration) -> PaginatedList[Repository]:
+    """Retrieve the list of repositories to analyse.
+
+    Returns:
+        PaginatedList[Repository]: The list of repositories.
+    """
+    github = Github(configuration.github_token)
+    repositories = github.search_repositories(
+        query=f"user:{configuration.repository_owner} archived:false"
+    )
+    logger.info(
+        "Retrieved repositories to analyse",
+        repositories_count=repositories.totalCount,
+        repositories=[repository.full_name for repository in repositories],
+    )
+    return repositories
+
