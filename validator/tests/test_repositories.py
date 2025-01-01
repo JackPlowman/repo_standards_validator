1befbfad0fa434d4b68613961f115f4c33545603
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-01-01 10:41:26 +0000

    update

---
+++ validator/tests/test_repositories.py
@@ -0,0 +1,19 @@
+from unittest.mock import patch
+
+from unittest.mock import MagicMock
+
+from validator.repositories import retrieve_repositories
+
+
+@patch("validator.repositories.Github")
+def test_retrieve_repositories(mock_github: MagicMock) -> None:
+    # Arrange
+    config = MagicMock()
+    # Act
+    result = retrieve_repositories(config)
+    # Assert
+    assert result == mock_github.return_value.search_repositories.return_value
+    mock_github.assert_called_once_with(config.github_token)
+    mock_github.return_value.search_repositories.assert_called_once_with(
+        query=f"user:{config.repository_owner} archived:false is:public"
+    )
