538e60517e09515d01cdda9d744ff19e08b004c5
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-01-01 09:29:29 +0000

    update

--- run.sh
+++ run.sh
@@ -12,6 +12,6 @@ python -m validator
 
 if [ "$CI" = "true" ]; then
   # if running in GitHub Actions, copy the output to the output directory
-  cp statistics/repositories.json github/workspace/repositories.json
-  echo "Copied statistics to github/workspace"
+  cp repositories.json github/workspace/repositories.json
+  echo "Copied repositories.json to github/workspace"
 fi
