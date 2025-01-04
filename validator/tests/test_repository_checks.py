e3114977d27412180ab629511a2e7bdc0958c47b
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-01-04 20:16:00 +0000

    update

--- validator/tests/test_repository_checks.py
+++ validator/tests/test_repository_checks.py
@@ -7,11 +7,12 @@ from validator.repository_checks import check_repository, status_to_bool
 
 def test_check_repository() -> None:
     # Arrange
-    repository = MagicMock(
-        secret_scanning_push_protection="enabled",  # noqa: S106
-        secret_scanning="enabled",  # noqa: S106
-        dependabot_security_updates="enabled",
-    )
+    repository = MagicMock()
+    repository.name = "test-repo"
+    repository.full_name = "owner/test-repo"
+    repository.security_and_analysis.secret_scanning_push_protection.status = "enabled"
+    repository.security_and_analysis.secret_scanning.status = "enabled"
+    repository.security_and_analysis.dependabot_security_updates.status = "enabled"
     # Act
     analysed_repository = check_repository(repository)
     # Assert
