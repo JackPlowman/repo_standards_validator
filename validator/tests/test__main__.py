ed70da7439fb6b43c4d890ca8e0145070c427964
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-01-04 20:27:42 +0000

    update

--- validator/tests/test__main__.py
+++ validator/tests/test__main__.py
@@ -8,7 +8,6 @@ FILE_PATH = "validator.__main__"
 @patch(f"{FILE_PATH}.set_up_custom_logging")
 @patch(f"{FILE_PATH}.Configuration")
 @patch(f"{FILE_PATH}.retrieve_repositories")
-@patch(f"{FILE_PATH}.check_repository")
 @patch(f"{FILE_PATH}.asdict")
 @patch(f"{FILE_PATH}.AnalysedRepositories")
 @patch(f"{FILE_PATH}.Path")
@@ -20,7 +19,6 @@ def test_main(
     mock_path: MagicMock,
     mock_analysed_repositories: MagicMock,
     mock_asdict: MagicMock,
-    mock_check_repository: MagicMock,
     mock_retrieve_repositories: MagicMock,
     mock_configuration: MagicMock,
     mock_set_up_custom_logging: MagicMock,
