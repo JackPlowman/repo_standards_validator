1f0fe957033b7c0a94e433b37db9e4bb3263a19c
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-01-24 20:51:20 +0000

    update

--- validator/tests/test__main__.py
+++ validator/tests/test__main__.py
@@ -1,13 +1,15 @@
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, call
 
-from validator.__main__ import main
+from validator.__main__ import main, clean_up
 
 FILE_PATH = "validator.__main__"
 
 
 @patch(f"{FILE_PATH}.set_up_custom_logging")
 @patch(f"{FILE_PATH}.Configuration")
+@patch(f"{FILE_PATH}.clean_up")
 @patch(f"{FILE_PATH}.retrieve_repositories")
+@patch(f"{FILE_PATH}.clone_repository")
 @patch(f"{FILE_PATH}.asdict")
 @patch(f"{FILE_PATH}.AnalysedRepositories")
 @patch(f"{FILE_PATH}.Path")
@@ -19,7 +21,9 @@ def test_main(
     mock_path: MagicMock,
     mock_analysed_repositories: MagicMock,
     mock_asdict: MagicMock,
+    mock_clone_repository: MagicMock,
     mock_retrieve_repositories: MagicMock,
+    mock_clean_up: MagicMock,
     mock_configuration: MagicMock,
     mock_set_up_custom_logging: MagicMock,
 ) -> None:
@@ -27,8 +31,10 @@ def test_main(
     main()
     # Assert
     mock_set_up_custom_logging.assert_called_once()
+    mock_clean_up.assert_has_calls([call(), call()])
     mock_configuration.assert_called_once_with()
     mock_retrieve_repositories.assert_called_once_with(mock_configuration.return_value)
+    mock_clone_repository.assert_not_called()
     mock_dump.assert_called_once()
     mock_generate_action_summary.assert_called_once()
     mock_path.assert_called_once_with("repositories.json")
@@ -37,3 +43,20 @@ def test_main(
         repositories=[],
     )
     mock_asdict.assert_not_called()
+@patch(f"{FILE_PATH}.Path")
+@patch(f"{FILE_PATH}.rmtree")
+def test_clean_up(
+    mock_rmtree: MagicMock,
+    mock_path: MagicMock,
+) -> None:
+    # Arrange
+    mock_path.return_value.iterdir.return_value = [
+        MagicMock(is_dir=lambda: True),
+        MagicMock(is_dir=lambda: False),
+    ]
+    # Act
+    clean_up()
+    # Assert
+    mock_path.assert_called_once_with("validator/cloned_repositories")
+    mock_path.return_value.iterdir.assert_called_once()
+    mock_rmtree.assert_called_once_with(mock_path.return_value.iterdir.return_value[0])
